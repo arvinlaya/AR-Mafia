14896fc9766442e0ae6ca74cbd564a6cf0ea8e46
arvinlaya283@gmail.com <>
2023-03-29 20:45:23 +0800

    set _maxPlayer = 5

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -33,8 +33,7 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     //Max player
     //Mali yung description, ito yung "# of players before you can START the game from the Room
-    private const int _maxPlayer = 4;
-
+    private const int _maxPlayer = 5;
     //START GAME
 
     //change to "Host can start" when min is met
