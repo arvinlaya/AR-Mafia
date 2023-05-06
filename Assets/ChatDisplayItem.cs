8d25f9224fa7a3890b486fa55b257cf579f12db9
bellosamuelb@gmail.com <>
2023-05-06 15:15:49 +0800

    Chat Reverted ayaw lang gumana nung Mafia Channel

--- Assets/ChatDisplayItem.cs
+++ Assets/ChatDisplayItem.cs
@@ -5,28 +5,25 @@ using UnityEngine;
 
 public class ChatDisplayItem : MonoBehaviour
 {
+    //TODO: Yung Avatar?
     [SerializeField] TMP_Text text;
-    [SerializeField] TMP_Text senderIGN;
-    [SerializeField] GameObject chatBubble;
 
     public void SetUp(string channelName, string[] senders, object[] messages)
     {
 
         if (messages[0].ToString().Contains("(MAFIA)"))
         {
-            senderIGN.color = Color.red;
-            senderIGN.text = string.Format(senders[0]);
-            text.text = string.Format("{0}", messages[0].ToString().Replace("(MAFIA)",""));
-
+            text.color = Color.red;
+            //sample:
+            //Player1(Mafia):Message Here
+            text.text = string.Format("{0}{1}", senders[0], messages[0].ToString().Replace("(MAFIA)","(Mafia):"));
         }
         else
         {
             //channel = ROOM from PUN2
-            senderIGN.color = Color.black;
-            //text.color = Color.white;
-
-            senderIGN.text = string.Format(senders[0]);
-            text.text = string.Format("{0}", messages[0], "(Mafia)");
+            text.color = Color.white;
+            text.text = string.Format("{0}: {1}", senders[0], messages[0], "(Mafia)");
         }
     }
 }
+
--- Assets/Scripts/Managers/PhotonChatManager.cs
+++ Assets/Scripts/Managers/PhotonChatManager.cs
@@ -24,7 +24,7 @@ public class PhotonChatManager : MonoBehaviour, IChatClientListener
     [SerializeField] string privateReceiver = "";
     [SerializeField] TMP_InputField chatField;
     //[SerializeField] TMP_Text chatDisplay;
-    [SerializeField] GameObject chatDisplayItemPrefab2;
+    [SerializeField] TMP_Text chatDisplayItemPrefab2;
 
     [SerializeField] GameObject firstChatMessageForMafia;
 
@@ -51,7 +51,7 @@ public class PhotonChatManager : MonoBehaviour, IChatClientListener
         {
             if (currentChat.Contains("/m"))
             {
-                chatClient.PublishMessage("MafiaCH", currentChat.Replace("/m", "(MAFIA)"));
+                chatClient.PublishMessage("MafiaCH", currentChat.Replace("/m", "(MAFIA)\n"));
             }
             else
             {
